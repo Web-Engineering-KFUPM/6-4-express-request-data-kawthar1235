cd7a7d69b12f64dd912fa483d8ae58924cd63970
kawthar1235 <s202168810@kfupm.edu.sa>
2026-04-08 12:41:19 +0300

    Query params

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -105,14 +105,30 @@ LAB SETUP INSTRUCTIONS
  */
 
 //import express
-
+import express from "express";
 
 // create express app instance to create web server
-
+const app = express();
 
 
 // Query params: /echo?name=Ali&age=22
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
 
+  if (!name || !age) {
+    return res.status(400).json({
+      ok: false,
+      error: "name & age required"
+    });
+  }
+
+  return res.json({
+    ok: true,
+    name,
+    age,
+    msg: `Hello ${name}, you are ${age}`
+  });
+});
 
 // Route params: /profile/First/Last
 
